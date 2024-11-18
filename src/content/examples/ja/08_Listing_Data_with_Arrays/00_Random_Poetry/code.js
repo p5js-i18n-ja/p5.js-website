67ddc35d616dbe972145113d8ab2f342a132bf29
SHIBAHARA Hiroki <shibomb@gmail.com>
2024-11-18 11:54:06 +0900

    translated: examples  08_Listing_Data_with_Arrays

--- src/content/examples/ja/08_Listing_Data_with_Arrays/00_Random_Poetry/code.js
+++ src/content/examples/ja/08_Listing_Data_with_Arrays/00_Random_Poetry/code.js
@@ -1,49 +1,125 @@
-// Define the global variables.
-// Create an array to store words from the p5 homepage statement.
-let words = ['p5.js', 'is', 'a', 'JavaScript', 'library', 'for', 'creative',
-  'coding', 'with', 'a', 'focus', 'on', 'making', 'coding', 'accessible', 'and',
-  'inclusive', 'for', 'artists', 'designers', 'educators', 'beginners', 'and',
-  'anyone', 'else!', 'p5.js', 'is', 'free', 'and', 'open-source', 'because', 
-  'we', 'believe', 'software', 'and', 'the', 'tools', 'to', 'learn', 'it',
-  'should', 'be', 'accessible', 'to', 'everyone', 'Using', 'the', 'metaphor',
-  'of', 'a', 'sketch', 'p5.js', 'has', 'a', 'full', 'set', 'of', 'drawing',
-  'functionality', 'However', "you're", 'not', 'limited', 'to', 'your',
-  'drawing', 'canvas', 'You', 'can', 'think', 'of', 'your', 'whole', 'browser',
-  'page', 'as', 'your', 'sketch', 'including', 'HTML5', 'objects', 'for', 'text',
-  'input', 'video', 'webcam', 'and', 'sound'];
+// グローバル変数を定義します。
+// p5のホームページのステートメントから単語を格納するための配列を作成します。
+let words = [
+  "p5.js",
+  "is",
+  "a",
+  "JavaScript",
+  "library",
+  "for",
+  "creative",
+  "coding",
+  "with",
+  "a",
+  "focus",
+  "on",
+  "making",
+  "coding",
+  "accessible",
+  "and",
+  "inclusive",
+  "for",
+  "artists",
+  "designers",
+  "educators",
+  "beginners",
+  "and",
+  "anyone",
+  "else!",
+  "p5.js",
+  "is",
+  "free",
+  "and",
+  "open-source",
+  "because",
+  "we",
+  "believe",
+  "software",
+  "and",
+  "the",
+  "tools",
+  "to",
+  "learn",
+  "it",
+  "should",
+  "be",
+  "accessible",
+  "to",
+  "everyone",
+  "Using",
+  "the",
+  "metaphor",
+  "of",
+  "a",
+  "sketch",
+  "p5.js",
+  "has",
+  "a",
+  "full",
+  "set",
+  "of",
+  "drawing",
+  "functionality",
+  "However",
+  "you're",
+  "not",
+  "limited",
+  "to",
+  "your",
+  "drawing",
+  "canvas",
+  "You",
+  "can",
+  "think",
+  "of",
+  "your",
+  "whole",
+  "browser",
+  "page",
+  "as",
+  "your",
+  "sketch",
+  "including",
+  "HTML5",
+  "objects",
+  "for",
+  "text",
+  "input",
+  "video",
+  "webcam",
+  "and",
+  "sound",
+];
 
-// Set the amount of words to be drawn on the canvas,   as
-// well as the starting hue value. Declare the position variable,
-// which will be used to randomly start the word selection in the array.
+// キャンバスに描画する単語の数と、
+// 開始色相値を設定します。配列内の単語選択をランダムに開始するための位置変数を宣言します。
 let wordCount = 15;
 let hue;
 let position;
 
 function setup() {
   describe(
-    'A random series of words related to p5.js scattered onto the canvas.'
+    "p5.jsに関連するランダムな単語の系列がキャンバスに散りばめられます。",
   );
 
-  // Import the selected font style defined in the canvas's style.css file.
-  textFont('Space Mono');
+  // キャンバスのstyle.cssファイルで定義されたフォントスタイルをインポートします。
+  textFont("Space Mono");
 
   createCanvas(720, 400);
 
-  // Set the text alignment to center and set the color mode to HSB.
+  // テキストの配置を中央揃えにし、色モードをHSBに設定します。
   textAlign(CENTER);
   colorMode(HSB);
 
-  // Define hue as a random value.
+  // 色相をランダムな値として定義します。
   hue = random(180, 360);
 
-  // Define the random starting point for selecting the words in the
-  // array.
+  // 配列内の単語を選択するためのランダムな開始点を定義します。
   position = floor(random(0, words.length - wordCount));
 
   background(hue, 95, 25);
 
-  // Draw as many words as set with the words variable in the
-  // canvas in random positions.
+  // words変数で設定された数だけ、キャンバスにランダムな位置で単語を描画します。
   for (let i = 0; i < 20; i++) {
     textSize(random(16, 48));
     fill(hue, 200, random(50, 95));
